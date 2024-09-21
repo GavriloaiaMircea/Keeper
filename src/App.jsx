296df001690ce278681c278730719b410cdc0320
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-09-21 16:45:10 +0300

    Created some components

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Note from "./Components/Note";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
