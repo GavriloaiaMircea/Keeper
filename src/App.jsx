8aabeb4d791ec6cdd9ac2f072df69c6e31d95be1
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-09-22 15:34:50 +0300

    Refactor Note component to render dynamic content

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Note from "./Components/Note";
+import notes from "./Notes";
 
 function App() {
   return (
     <>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </>
   );
